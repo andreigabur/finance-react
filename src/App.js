6db55a9434c1c6bcd4be2a4170a096c3545a7b4a
andrei.gabur <andrei.gabur@servicenow.com>
2017-10-26 15:50:11 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<header className="app-header">
+					<img src="and.jpeg" className="img-circle" alt="logoAndrei" />
+					<img src={logo} className="app-logo" alt="logo" />
+					<img src="ben.jpeg" className="img-circle" alt="logoBenedetto" />
+					<h1 className="app-title">Manage your <span>Finance</span> with <span>React</span></h1>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
